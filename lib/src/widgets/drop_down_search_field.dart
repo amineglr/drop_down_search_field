162166790bf5c712cb7042cf3ca0d0597081c818
amineglr <aminedglrglr@gmail.com>
2024-04-26 14:02:43 +0300

    dropdown translate

--- lib/src/widgets/drop_down_search_field.dart
+++ lib/src/widgets/drop_down_search_field.dart
@@ -852,9 +852,8 @@ class _DropDownSearchFieldState<T> extends State<DropDownSearchField<T>>
             },
             child: _suggestionsBox!.direction == AxisDirection.down
                 ? suggestionsList
-                : FractionalTranslation(
-                    transformHitTests: false,
-                    translation:
+                : Transform.translate(
+                    offset:
                         const Offset(0.0, -1.0), // visually flips list to go up
                     child: suggestionsList,
                   )),
